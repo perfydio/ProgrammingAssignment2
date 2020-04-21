986df8d5a29dd227ef58b40af830cf6663a19a55
Juan Luis Andión Tápiz <57728481+perfydio@users.noreply.github.com>
2020-04-21 01:00:05 +0000

    Assigment 2 solution
    Here is the two functions required for to complete the Assigment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here is two functions: the first function create a special matrix and 
+## its inverse; the second function gets the inverse from cache and skips
+## the computation. Otherwise, it calculates the inverse of the matrix  
+## and sets the value of the inverse in the cache via the setsolve function
 
-## Write a short comment describing this function
+## This function create the special matrix which is really a list
+## containing a funtion to: 
+## 1: set the value of the matrix; 2: get the value of the matrix;
+## 3: set the value of the inverse;4: get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<- NULL
+        set<-function(y){
+                x<<-y
+                inv<<-NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse and set it in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(inv)) {
+                message("getting cached matrix")
+                return(inv)
+        }
+        data <- x$get()
+        m <- mean(matrix, ...)
+        x$setsolve(inv)
+        inv
 }
